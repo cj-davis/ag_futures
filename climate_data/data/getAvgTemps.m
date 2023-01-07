9e7eeb1461f1fc88b74a03afe5914a1bef23d0a8
Cole Davis <coda5338@colorado.edu>
2023-01-07 16:51:55 -0700

    updates

--- climate_data/data/getAvgTemps.m
+++ climate_data/data/getAvgTemps.m
@@ -8,14 +8,19 @@ function [ret_temp, min_date, ws] = getAvgTemps(state)
     % Open the state's excel file (note that this requires the data file to
     % be in the working directory)
     data = readtable(strcat(state, '.xlsx'));
+    % Create dates array with date data
     dates = table2array(data(:, "YEARMODA"));
+    % Create stations array with station IDs
     stations = table2array(data(:,"STN"));
+    % Create temps array with daily temperatures
     temps = table2array(data(:,"TEMP"));
+    % Starting date yyyymmdd
     min_date = 19730101;
+    max_date = 20201231;
     ws = unique(stations);
     len = length(temps);
-    % Initialize average temperatures vector
-    tot_time = timeElapsed(19730101, 20201231);
+    % Initialize average temperatures matrix
+    tot_time = timeElapsed(min_date, max_date);
     avg_temp = zeros(tot_time, length(ws));
     rows = 1;
     cols = 1;
@@ -30,41 +35,37 @@ function [ret_temp, min_date, ws] = getAvgTemps(state)
         date_str = date_str(5:8);
         
         if ~strcmp(date_str, '0229') % We will exclude leap year data for simplicity
-            if i==1 && min_date < dates(i)
-                % Then the latest first recorded date happens after 19730101
-                min_date = dates(i);
-            elseif i==1 && dates(i) ~= 19730101 
+            if (i==1) && (dates(i) ~= min_date) 
                 % then our rows index needs to be greater than 1
-                rows = timeElapsed(19730101, dates(i));
+                rows = timeElapsed(min_date, dates(i));
             end
             
             if i>1 && (dates(i-1) > dates(i) || stations(i-1) ~= stations(i))
             % Then we are looking at data for a new weather station
                 % Update avg_temp indices
                 cols = cols + 1;
-                if dates(i) ~= 19730101 % then our rows index needs to be greater than 1
-                    rows = timeElapsed(19730101, dates(i));
+                if dates(i) ~= min_date % then our rows index needs to be greater than 1
+                    rows = timeElapsed(min_date, dates(i));
                 else
                     rows = 1;
-                end
-
-                if min_date < dates(i) % Then the latest first recorded date happens after min_date
-                    min_date = dates(i);
                 end
             end
         end
         
+        if rows >= 17520
+            fprintf('error at WS: %d\n', stations(i));
+            fprintf('date occurred: %d\n', dates(i));
+        end
+        
         % Record average temperature data at station cols and time rows
-        fprintf('%d\n', rows);
         ws_per_day(rows) = ws_per_day(rows) + 1;
         avg_temp(rows, cols) = temps(i);
         rows = rows+1;
         
     end
     
-    ret_temp = zeros((2020-1973 + 1) * 365,1);
-    num_days = (2020-1973 + 1) * 365;
-    for i=1:num_days
+    ret_temp = zeros(tot_time,1);
+    for i=1:tot_time
         % Compute daily sample means
         ret_temp(i) = sum(avg_temp(i, :)) / ws_per_day(i);
     end
